06d8e684485a7fc45d2a89707cfc9ce4538c6bb7
Waga <wagaodongo@gmail.com>
2020-01-11 10:15:13 +0300

    minor change

--- Wesplit/ContentView.swift
+++ Wesplit/ContentView.swift
@@ -16,17 +16,31 @@ struct ContentView: View {
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        
+        NavigationView{
         Form {
             Section{
                 TextField("Amount", text: $checkAmount)
                     .keyboardType(.decimalPad)
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2..<100) {
+                        Text("\($0) people")
+                    }
+                }
             }
+            
             Section {
-                Text("\(checkAmount)")
+                Picker("Tip percentage", selection: $tipPercentage) {
+                    ForEach(0..<tipPercentages.count) {
+                        Text("\(self.tipPercentages[$0])%")
+                    }
+                }
+            .pickerStyle(SegmentedPickerStyle())
             }
+           
+        }.navigationBarTitle("Split")
         }
-        }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
